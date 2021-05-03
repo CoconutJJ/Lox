e0b5953ef2804938299a8a3a2d1892162691f428
David Yue <davidyue5819@gmail.com>
2021-05-03 08:06:07 -0700

    Add GPL license header

--- token/token.c
+++ token/token.c
@@ -1,3 +1,18 @@
+/*
+    This program is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the Free
+    Software Foundation, either version 3 of the License, or (at your option)
+    any later version.
+
+    This program is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+    more details.
+
+    You should have received a copy of the GNU General Public License along with
+    this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -57,14 +72,14 @@ void add_token(TOKEN_T t, char *value, int line) {
         return;
 }
 
-void destroyTokenList(TOKEN *head) { 
+void destroyTokenList(TOKEN *head) {
         while (head) {
-                TOKEN * next = head->next;
+                TOKEN *next = head->next;
                 free(head);
                 head = next;
-        } 
+        }
         start = NULL;
-        end = NULL;
+        end   = NULL;
 }
 
 TOKEN_T matchIdentifier(char *ident) {
