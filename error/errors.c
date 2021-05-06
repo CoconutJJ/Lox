537e240841cc296848552026c7e307135b52460c
David Yue <davidyue5819@gmail.com>
2021-05-06 09:17:37 -0700

    removed is panic handling code

--- error/errors.c
+++ error/errors.c
@@ -17,32 +17,7 @@
 
 #include "../types.h"
 
-static int is_panic_mode = 0;
-
-int is_panic() { return is_panic_mode; }
-
-void panic(TOKEN **current) {
-        is_panic_mode = 1;
-
-        while ((*current)) {
-                switch ((*current)->t) {
-                case IF:
-                case WHILE:
-                        goto panic_end;
-                        break;
-                default:
-                        (*current) = (*current)->next;
-                        break;
-                }
-        }
-
-panic_end:
-
-        return;
-}
-
 void register_error(ERROR_T type, char *msg, int line) {
-        if (is_panic()) return;
 
         switch (type) {
         case SYNTAX_ERROR:
@@ -56,7 +31,6 @@ void register_error(ERROR_T type, char *msg, int line) {
 }
 
 void parse_error(char *msg, int line) {
-        if (is_panic()) exit(EXIT_FAILURE);
 
         fprintf(stderr, "parse error: line %d: %s", line, msg);
         exit(EXIT_FAILURE);
